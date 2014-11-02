142d8db2864d717d6ac063647a1ef311e9102b2b
neonsoftware <frankie@frankie.com>
2014-11-02 20:54:21 +0100

    Settings back to normal

--- apply/apply/settings.py
+++ apply/apply/settings.py
@@ -64,12 +64,15 @@ DATABASES = {
     'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
     'NAME': 'yard',                      # Or path to database file if using sqlite3.
     'USER': 'frankie',                      # Not used with sqlite3.
-    'PASSWORD': 'frankiefrankie',                  # Not used with sqlite3.
-    'HOST': 'archivione.caprj3rjkmkr.us-west-2.rds.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
-    'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
+    'PASSWORD': 'frankie',                  # Not used with sqlite3.
+    'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+    'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
   }
 }
 
+#'PASSWORD': 'frankie',                  # Not used with sqlite3.
+#'HOST': 'archivione.caprj3rjkmkr.us-west-2.rds.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
+#'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
 
 
 # Internationalization
