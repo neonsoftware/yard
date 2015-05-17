2451011950768d192308fc221cf460c59f40c621
testy <testy@testy.com>
2015-05-17 18:40:53 +0200

    Updated admin

--- apply/yard/admin.py
+++ apply/yard/admin.py
@@ -3,10 +3,32 @@ from django.contrib import admin
 from yard.models import Skill, Cover, Company, Application, Profile, Piece, PieceCategory
 
 # Register your models here.
-admin.site.register(Skill)
+
+class SkillAdmin(admin.ModelAdmin):
+    fields = ['user', 'name']
+    list_display = ('user', 'name')
+
+class PieceCategoryAdmin(admin.ModelAdmin):
+    fields = ['user', 'name', 'description', 'language', 'tags', 'pieces']
+    list_display = ('user', 'name', 'description', 'language', 'tags', 'pieces')
+
+class PieceAdmin(admin.ModelAdmin):
+    fields = ['user', 'content', 'language', 'tags', 'legend']
+    list_display = ('user', 'content', 'language', 'tags', 'legend')
+
+class ApplicationAdmin(admin.ModelAdmin):
+    fields = ['user', 'position', 'company']
+    list_display = ('user', 'position', 'company')
+
+class CoverAdmin(admin.ModelAdmin):
+    fields = ['user', 'name', 'content']
+    list_display = ('user', 'name', 'content')
+
+
+admin.site.register(Skill, SkillAdmin)
+admin.site.register(PieceCategory, PieceCategoryAdmin)
+admin.site.register(Piece, PieceAdmin)
 admin.site.register(Company)
-admin.site.register(Application)
+admin.site.register(Application, ApplicationAdmin)
 admin.site.register(Profile)
-admin.site.register(Piece)
-admin.site.register(PieceCategory)
-admin.site.register(Cover)
+admin.site.register(Cover, CoverAdmin)
